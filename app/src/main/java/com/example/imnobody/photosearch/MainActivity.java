d2918a47cf534e9f5f9dc1a83761757862cdb2c7
Ankit Anand <aanand29@asu.edu>
2018-02-10 17:12:09 -0700

    Initial commit

---
+++ app/src/main/java/com/example/imnobody/photosearch/MainActivity.java
@@ -0,0 +1,237 @@
+
+
+package com.example.imnobody.photosearch;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private ImageGridAdapter imageGridAdapter;
+    private List<String> imageList;
+    private String searchString;
+    private TextView emptyTextView;
+    private View loadingIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageList = new ArrayList<>(100);
+
+        //set up gridView and it's empty case view
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        emptyTextView = (TextView) findViewById(R.id.empty);
+        gridView.setEmptyView(emptyTextView);
+
+        loadingIndicator = findViewById(R.id.loading_indicator_main_grid);
+
+        imageGridAdapter = new ImageGridAdapter(MainActivity.this, imageList);
+
+        gridView.setAdapter(imageGridAdapter);
+
+
+      /*
+       TODO: Rotation Fix Volley
+
+       if (savedInstanceState != null) {
+            savedsearchString = savedInstanceState.getString("searchStringKey");
+            if (savedsearchString!=null && !savedsearchString.isEmpty()) {
+                searchString = savedsearchString;
+                loadingIndicator.setVisibility(View.VISIBLE);
+                emptyTextView.setVisibility(View.INVISIBLE);
+                volleyRequest(getSearchString(),0);
+            }
+        }*/
+
+
+        gridView.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                loadNextDataFromApi(page);
+                return true;
+            }
+        });
+
+        //listener for each item in gridView
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if ((!Utils.checkImageResource(MainActivity.this, (ImageView) view.findViewById(R.id.grid_item_image), R.drawable.ic_image_error)) && ((ProgressBar)view.findViewById(R.id.grid_item_loading_indicator)).getVisibility()==View.INVISIBLE) {
+                        Intent intent = new Intent(MainActivity.this, ImageActivity.class);
+                        intent.putExtra("imageuri", imageList.get(position));
+                        startActivity(intent);
+                } else if (Utils.checkImageResource(MainActivity.this, (ImageView) view.findViewById(R.id.grid_item_image), R.drawable.ic_image_error)) {
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.error_loading), Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,getResources().getString(R.string.image_loading), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    // This method probably sends out a network request and appends new data items to your adapter.
+    public void loadNextDataFromApi(int offset) {
+        Uri.Builder uriBuilder = Utils.getUri(getSearchString());
+        uriBuilder.appendQueryParameter("start", "" + offset);
+        volleyRequest(uriBuilder.toString(), 1);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+
+        // Set current activity as searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        return true;
+    }
+
+    // Receive query from searchWidget
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    // Handle query from seacrchWidget
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            searchString = intent.getStringExtra(SearchManager.QUERY);
+            loadingIndicator.setVisibility(View.VISIBLE);
+            emptyTextView.setVisibility(View.INVISIBLE);
+            Log.d(TAG,"Uri is " +getUri());
+            volleyRequest(getUri(), 0);
+        }
+    }
+
+    //Volley request for json string
+    public void volleyRequest(String volleySearchString, final int addFlag) {
+
+        /*
+         * @params {Request type, url to be searched, responseHandler, errorHandler}
+         */
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, volleySearchString,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //Log.d(TAG,"response is :- "+ response);
+                        loadingIndicator.setVisibility(View.GONE);
+                        updateUIPostExecute(Utils.extractImages(response), addFlag);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                loadingIndicator.setVisibility(View.GONE);
+                imageGridAdapter.clear();
+                emptyTextView.setVisibility(View.VISIBLE);
+
+                String message = null;
+                if (error instanceof NetworkError) {
+                    message = getResources().getString(R.string.connection_error);
+                } else if (error instanceof ServerError) {
+                    message = getResources().getString(R.string.server_error);
+                } else if (error instanceof AuthFailureError) {
+                    message = getResources().getString(R.string.connection_error);
+                } else if (error instanceof ParseError) {
+                    message = getResources().getString(R.string.parse_error);
+                } else if (error instanceof TimeoutError) {
+                    message = getResources().getString(R.string.timeout_error);
+                }
+
+                emptyTextView.setText(message);
+            }
+
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Client-ID 41a1c63ff97b89d");
+                return headers;
+            }
+        };
+
+        VolleySingleton.getInstance(this.getApplicationContext()).addToRequestQueue(stringRequest);
+    }
+
+    public void updateUIPostExecute(List<String> response, int addFlag) {
+
+
+        if (addFlag == 0) {
+            imageGridAdapter.clear();
+            imageList.clear();
+        }
+        imageList.addAll(response);
+        imageGridAdapter.notifyDataSetChanged();
+
+    }
+
+    /*
+    TODO: Rotation Fix Volley
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        if (searchString != null && !searchString.isEmpty()) {
+            outState.putString("searchStringKey", searchString);
+        } else if(savedsearchString != null && !savedsearchString.isEmpty()){
+                outState.putString("searchStringKey", savedsearchString);
+        }
+        super.onSaveInstanceState(outState);
+    }*/
+
+
+    public String getSearchString() {
+        return searchString;
+    }
+
+    public String getUri() {
+        return Utils.getUri(getSearchString()).toString();
+    }
+}
---
+++ app/src/main/java/com/example/imnobody/photosearch/Utils.java
@@ -0,0 +1,109 @@
+package com.example.imnobody.photosearch;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Utils {
+
+    private static final String TAG = Utils.class.getName();
+    private static final String BASEURL = "https://api.imgur.com/3/gallery/search/";
+
+    private Utils() {
+    }
+
+    //make URL from base url and userText
+    public static Uri.Builder getUri(String query) {
+        Uri baseuri = Uri.parse(BASEURL);
+        Uri.Builder uriBuilder = baseuri.buildUpon();
+        uriBuilder.appendQueryParameter("q", query);
+        return uriBuilder;
+    }
+
+    // Extract json from string response
+
+    public static List<String> extractImages(String sampleJsonResponse) {
+
+        List<String> imageUrlList = new ArrayList<>();
+
+        try {
+
+            JSONObject baseJsonResponse = new JSONObject(sampleJsonResponse);
+
+            JSONArray itemsArray = baseJsonResponse.getJSONArray("data");
+            if (itemsArray != null) {
+
+                Log.d(TAG, "images is " + itemsArray.length());
+                for (int i = 0; i < itemsArray.length(); i++) {
+
+                    JSONObject itemsArrayJSONObject = itemsArray.getJSONObject(i);
+                    JSONArray imagesJSONArray = null;
+                    if (itemsArrayJSONObject.has("images"))
+                        imagesJSONArray = itemsArrayJSONObject.getJSONArray("images");
+
+                /*
+                 * To retrieve  thumbnails -- omitted
+                 *
+                 * JSONObject imageJSONObject = itemsArrayJSONObject.getJSONObject("image");
+                 * imageUrl.add(imageJSONObject.getString("thumbnailLink"));
+                 */
+                    JSONObject imagesJSONObject = null;
+                    if (imagesJSONArray != null) {
+                        Log.d("Ankit", "images is " + imagesJSONArray.length());
+                        imagesJSONObject = imagesJSONArray.getJSONObject(0);
+                    }
+                    if (imagesJSONObject != null && imagesJSONObject.has("link")){
+                        imageUrlList.add(imagesJSONObject.getString("link"));
+                    }
+                }
+            }
+            } catch(JSONException e){
+                Log.e("Utils", "Problem parsing", e);
+            }
+
+        // return list of image urls after parsing
+        Log.d(TAG, " "+ imageUrlList);
+        return imageUrlList;
+
+    }
+
+    // get resource from imageview
+    @SuppressWarnings("deprecation")
+    @SuppressLint("NewApi")
+    public static boolean checkImageResource(Context ctx, ImageView imageView, int imageResource) {
+        boolean result = false;
+
+        if (ctx != null && imageView != null && imageView.getDrawable() != null) {
+            Drawable.ConstantState constantState;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                constantState = ctx.getResources()
+                        .getDrawable(imageResource, ctx.getTheme())
+                        .getConstantState();
+            } else {
+                constantState = ctx.getResources().getDrawable(imageResource)
+                        .getConstantState();
+            }
+
+            if (imageView.getDrawable().getConstantState() == constantState) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+
+
+}
